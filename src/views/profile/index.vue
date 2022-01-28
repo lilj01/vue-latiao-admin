a41b7c277fb848588bf5d9700d59993685b0e666
lilj <bclzxc@163.com>
2022-01-28 10:32:56 +0800

    element-plus国际化处理

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="">个人中心 {{ $t('msg.test') }}</div>
+  <div class="">
+    个人中心 {{ $t('msg.test') }}
+    <el-pagination
+      :page-size="100"
+      layout="total, prev, pager, next"
+      :total="1000"
+    >
+    </el-pagination>
+  </div>
 </template>
 
 <script setup>
